2560cf4907be590c4eec9d0d37bd46f6cd14b939
Santiago VdeK <santiago.vdk@gmail.com>
2017-11-05 18:42:32 -0600

    Agregar .ino

--- driver/test.c
+++ driver/test.c
@@ -6,18 +6,31 @@
 FILE * device;   //pointer for a file handled to driver
 
 void main(){
+	mov(1,0,"3200");
+}
+
+int mov(int motor, int direccion, char * steps){
+
 	device = fopen("/dev/arduino","r+");
 	if(device != NULL){
-		
-		fprintf(device, "%c", 'p');
-		sleep(2);
-fprintf(device, "%c", 'a');
+		if(direccion == 0) {
+			fprintf(device, "%s", "mov_m1_izq_" + steps + "\n");
+			fclose(device);
+			return 0;
+		}
 
+		fprintf(device, "%s", "mov_m1_izq_" + steps + "\n");
 		fclose(device);
+		return 0;	
+		
 	}
 	else{
 		printf("File no found\n");
+		return -1;
 	}
 
 }
+
+
+
 
